3d964f71f9d1f735a33b916f8fd47708278be1de
draddo11 <38097150+draddo11@users.noreply.github.com>
2020-05-14 16:44:43 +0000

    added new event list

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -5,12 +5,29 @@
   </div>
 </template>
 
+
 <script>
 import EventCard from '@/components/EventCard.vue'
+import axios from 'axios' // <--- brings in the axios library
 
 export default {
   components: {
     EventCard
+  },
+  data() {
+    return {
+      events: []
+    }
+  },
+  created() {
+    axios
+      .get('http://localhost:3000/events') // Does a get request
+      .then(response => {
+        this.events = response.data // For now, logs out the response
+      })
+      .catch(error => {
+      console.log('There was an error:', error.response) // Logs out the error
+      })
   }
 }
 </script>
