bad0578f5b5760ad52a2584e79a1e1be9a3b49dc
draddo11 <38097150+draddo11@users.noreply.github.com>
2020-05-29 22:22:34 +0000

    actions added fixed

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -3,9 +3,10 @@
   <h1>Create Event, {{user.name}}</h1>
   <p>This event is created by {{ user.name }}</p>
   <!-- <h1>Create Event, {{this.$store.categories}}</h1> -->
-  <p>{{ getEventById(2) }}</p>
-  <button @click="incrementCount">Increment</button>
-  <input type="number" v-model.number="incrementBy">
+  <!-- <p>{{ getEventById(2) }}</p> -->
+  <!-- <button @click="incrementCount">Increment</button> -->
+  <!-- <input type="number" v-model.number="incrementBy"> -->
+  <!-- <p>{{ getEvent(1) }}</p> -->
 </div>
 </template>
 <script>
@@ -14,11 +15,14 @@ import { mapState, mapGetters } from 'vuex'
 export default {
   computed: {
     ...mapGetters(['getEventById']),
-    ...mapState(['user', 'categories'])
+    ...mapState(['user', 'categories']),
+    getEvent() {
+      return this.$store.getters.getEventById
+    }
   },
   method: {
     incrementCount() {
-      this.$store.commit('INCREMENT_COUNT',this.incrementBy)
+      this.$store.commit('INCREMENT_COUNT', this.incrementBy)
     }
   }
 }
